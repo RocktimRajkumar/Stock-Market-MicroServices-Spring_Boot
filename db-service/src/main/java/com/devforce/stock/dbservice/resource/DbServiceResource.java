8154408f8df80abc8e7a1f2784c6677ba77407f7
Rocktim <rajkumar.rocktim@gmail.com>
2019-12-23 00:36:46 +0530

    add Quotes done

--- db-service/src/main/java/com/devforce/stock/dbservice/resource/DbServiceResource.java
+++ db-service/src/main/java/com/devforce/stock/dbservice/resource/DbServiceResource.java
@@ -1,11 +1,9 @@
 package com.devforce.stock.dbservice.resource;
 
 import com.devforce.stock.dbservice.model.Quote;
+import com.devforce.stock.dbservice.model.Quotes;
 import com.devforce.stock.dbservice.repository.QuotesRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,9 +21,22 @@ public class DbServiceResource {
     @GetMapping("/{username}")
     public List<String> getQuotes(@PathVariable("username") final String username) {
 
+        return getQuotesByUserName(username);
+    }
+
+    private List<String> getQuotesByUserName(@PathVariable("username") String username) {
         return quotesRepository.findByUserName(username)
                 .stream()
                 .map(Quote::getQuote)
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping("/add")
+    public List<String> add(@RequestBody final Quotes quotes) {
+        quotes.getQuotes()
+                .stream()
+                .map(quote -> new Quote(quotes.getUserName(), quote))
+                .forEach(quote -> quotesRepository.save(quote));
+        return getQuotes(quotes.getUserName());
     }
 }
